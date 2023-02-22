7ce057ba5e214c0903ea4cb93e6ee569c74277d7
sc4rfurry <usama.bsse3255@iiu.edu.pk>
2023-02-22 17:24:14 +0500

    fix :: Fixed binary path (AsnMap)
    Now Check for the path where it downloads the binary if it's not there it tries to create the path.

--- modules/cl4w_asnlookup.py
+++ modules/cl4w_asnlookup.py
@@ -6,7 +6,7 @@ from rich.panel import Panel
 from rich.console import Console
 from subprocess import check_output, PIPE
 from shlex import split
-from os import path, getcwd, name as nm, path, chmod
+from os import path, getcwd, name as nm, path, chmod, makedirs
 from wget import download
 from zipfile import ZipFile
 
@@ -69,14 +69,50 @@ class AsnLookup:
             console.print("[bold red]ASN not found![/bold red]")
             exit(1)
 
+    def check_dir_struct(self):
+        if os == "nt":
+            dir_path = path.join(wd, "core", "arsenal")
+            dir_path = r'"' + dir_path + r'"'
+        else:
+            dir_path = path.join(wd, "core", "arsenal")
+        try:
+            if path.exists(dir_path) and path.isdir(dir_path):
+                return True
+            else:
+                print(
+                    f"\n{bcolors.OKCYAN}[*] Creating directory structure...{bcolors.ENDC}")
+                print(
+                    f"\n{bcolors.OKCYAN}[*] Creating {bin_path}...{bcolors.ENDC}")
+                makedirs(bin_path)
+                return True
+        except Exception as e:
+            console.print(f"\n[bold red]Error: {e}[/bold red]")
+            exit(1)
+        except KeyboardInterrupt:
+            console.print("\n[yellow bold]" + "[~] " + "[/yellow bold]",
+                          "[red bold]Keyboard Interrupted[/red bold]", style="blink")
+            exit(1)
+
     def download_bins(self):
         if os == "nt":
             try:
+                if not path.exists(bin_path):
+                    try:
+                        makedirs(bin_path)
+                    except Exception as e:
+                        console.print(f"\n[bold red]Error: {e}[/bold red]")
+                        exit(1)
+                    except KeyboardInterrupt:
+                        console.print("\n[yellow bold]" + "[~] " + "[/yellow bold]",
+                                      "[red bold]Keyboard Interrupted[/red bold]", style="blink")
+                        exit(1)
+                else:
+                    pass
                 __path = path.join(bin_path, "asnmap.exe")
                 __path__ = r'"' + __path__ + r'"'
                 if path.exists(__path__) and path.isfile(__path__):
                     print(
-                        f"\t\t{bcolors.OKGREEN}[*] AsnMap found..!{bcolors.ENDC}")
+                        f"\n\t{bcolors.OKGREEN}[*] AsnMap found..!{bcolors.ENDC}")
                     return True
                 else:
                     print(
@@ -98,7 +134,7 @@ class AsnLookup:
                 __path = path.join(bin_path, "asnmap")
                 if path.exists(__path) and path.isfile(__path):
                     print(
-                        f"\t\t{bcolors.OKGREEN}[*] AsnMap found..!{bcolors.ENDC}")
+                        f"\n\t{bcolors.OKGREEN}[*] AsnMap found..!{bcolors.ENDC}")
                     return True
                 else:
                     print(
@@ -125,19 +161,24 @@ class AsnLookup:
         asnmap = path.join(bin_path, "asnmap")
         if os == "nt":
             try:
-                if self.download_bins():
-                    asnmap = asnmap + ".exe"
-                    asnmap = r'"' + asnmap + r'"'
-                    _output = check_output(
-                        split(f"{asnmap} -a {self.asn} -silent"), stderr=PIPE, stdin=PIPE)
-                    console.print("\n", Panel.fit(f"""
-[bold yellow3]ASN:[/bold yellow3] {self._asn[0]}
-[bold yellow3]Lookup Details:[/bold yellow3]
+                if self.check_dir_struct():
+                    if self.download_bins():
+                        asnmap = asnmap + ".exe"
+                        asnmap = r'"' + asnmap + r'"'
+                        _output = check_output(
+                            split(f"{asnmap} -a {self.asn} -silent"), stderr=PIPE, stdin=PIPE)
+                        console.print("\n", Panel.fit(f"""
+    [bold yellow3]ASN:[/bold yellow3] {self._asn[0]}
+    [bold yellow3]Lookup Details:[/bold yellow3]
 
 {_output.decode("utf-8")}""", title="[bold cyan]ASN Lookup[/bold cyan]", border_style="bold red", padding=(0, 2), title_align="center"))
+                    else:
+                        console.print(
+                            f"\n[bold red]Error: Binaries are missing.[/bold red]")
+                        exit(1)
                 else:
                     console.print(
-                        f"\n[bold red]Error: Binaries are missing.[/bold red]")
+                        f"\n[bold red]Error: Directory structure is missing.[/bold red]")
                     exit(1)
             except Exception as e:
                 console.print(f"\n[bold red]Error: {e}[/bold red]")
@@ -148,19 +189,24 @@ class AsnLookup:
                 exit(1)
         else:
             try:
-                if self.download_bins():
-                    perms = "777"
-                    chmod(asnmap, int(perms, 8))
-                    _output = check_output(
-                        split(f"{asnmap} -a {self.asn} -silent"), stderr=PIPE, stdin=PIPE)
-                    console.print("\n", Panel.fit(f"""
-[bold yellow3]ASN:[/bold yellow3] {self._asn[0]}
-[bold yellow3]Lookup Details:[/bold yellow3]
+                if self.check_dir_struct():
+                    if self.download_bins():
+                        perms = "777"
+                        chmod(asnmap, int(perms, 8))
+                        _output = check_output(
+                            split(f"{asnmap} -a {self.asn} -silent"), stderr=PIPE, stdin=PIPE)
+                        console.print("\n", Panel.fit(f"""
+    [bold yellow3]ASN:[/bold yellow3] {self._asn[0]}
+    [bold yellow3]Lookup Details:[/bold yellow3]
 
 {_output.decode("utf-8")}""", title="[bold cyan]ASN Lookup[/bold cyan]", border_style="bold red", padding=(0, 2), title_align="center"))
+                    else:
+                        console.print(
+                            f"\n[bold red]Error: Binaries are missing.[/bold red]")
+                        exit(1)
                 else:
                     console.print(
-                        f"\n[bold red]Error: Binaries are missing.[/bold red]")
+                        f"\n[bold red]Error: Directory structure is missing.[/bold red]")
                     exit(1)
             except Exception as e:
                 console.print(f"\n[bold red]Error: {e}[/bold red]")
